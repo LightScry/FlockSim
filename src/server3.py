cdc6ef0c1dce2ef490f27169b02c9814f6793460
Stefan Anders Mellem <kadish@server.fake>
2013-12-06 01:25:34 -0600

    Added a server script compatible with Python 3

---
+++ src/server3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import http.server 
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_my_headers()
+
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+
+
+if __name__ == '__main__':
+    http.server.test(HandlerClass=MyHTTPRequestHandler)
